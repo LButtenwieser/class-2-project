6f8ad2070fbb2fcb0eda67ff72dfe95319a9aa2d
LButtenwieser <lwb3@nyu.edu>
2024-05-14 05:35:34 -0400

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+$(":button").on("click", function () {
+    $("#textToChange").remove();
+})
+
+$("#Pink-Floyd").on("click", function () {
+    window.open("https://www.youtube.com/watch?v=nVhNCTH8pDs", '_blank');
+})
+
+$("#Tom-Petty").on("click", function () {
+    window.open("https://www.youtube.com/watch?v=s5BJXwNeKsQ", '_blank');
+
+})
+
+$("#Foo-Fighters").on("click", function () {
+    window.open("https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.youtube.com/watch%3Fv%3D1VQ_3sBZEm0&ved=2ahUKEwjrxMW-gomGAxW8ElkFHaMtDhsQtwJ6BAgMEAI&usg=AOvVaw0ySDI4Klt-6L6d4Cl3hOyg", '_blank');
+ 
+})
